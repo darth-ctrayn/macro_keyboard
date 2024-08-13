72110724cdfa4747245b7952dfb64a9b179ddc3a
calvin.passmore <calvin.passmore@wavetronix.com>
2024-08-13 16:00:45 -0600

    Sending some keys correctly

--- macro_keyboard.py
+++ macro_keyboard.py
@@ -1,36 +1,111 @@
-import keyboard, keyboard.mouse
+# import keyboard, keyboard.mouse
 import yaml
 import time
+import multiprocessing as mp
+from read_keypad import *
 # from macros_verify import *
 
 MACRO_FILE = "macros.yaml"
+NULL_CHAR = chr(0)
+
+transform_key = {
+    # String keys
+    'a': chr(4),
+    'b': chr(5),
+    'c': chr(6),
+    'd': chr(7),
+    'e': chr(8),
+    'f': chr(9),
+    'h': chr(10),
+    'i': chr(11),
+    'j': chr(12),
+    'k': chr(13),
+    'l': chr(14),
+    'm': chr(15),
+    'n': chr(16),
+    'o': chr(17),
+    'p': chr(18),
+    'q': chr(19),
+    'r': chr(20),
+    's': chr(21),
+    't': chr(22),
+    'u': chr(23),
+    'v': chr(24),
+    'w': chr(25),
+    'x': chr(26),
+    'y': chr(28),
+    'z': chr(29),
+    '1': chr(30),
+    '2': chr(31),
+    '3': chr(32),
+    '4': chr(33),
+    '5': chr(34),
+    '6': chr(35),
+    '7': chr(36),
+    '8': chr(37),
+    '9': chr(38),
+    '0': chr(39),
+    ' ': chr(44),
+    # Control keys
+    'ret': chr(40), #
+    'esc': chr(41),
+    'del': chr(42), #
+    'tab': chr(43), #
+    'cap': chr(57), #
+    'f1':  chr(58),
+    'right_arrow': chr(0x4F),
+    'left_arrow':  chr(0x50), #
+    'down_arrow':  chr(0x51), #
+    'up_arrow':    chr(0x52), #
+    NULL_CHAR: NULL_CHAR
+}
+
+def send_thread( queue, macros ):
+    print(macros)
+    while True:
+        keys = queue.get()
+        print(f'Got {keys}')
+        if keys in macros:
+            print(f"Handling macro {macros[keys]}")
+            handle_macro( macros[keys] )
 
 def handle_macro( steps ):
     for step in steps:
         if step['type'] == 'key':
             for key in step['keys']:
-                keyboard.send(key)
+                send_key(key)
+            send_key(NULL_CHAR)
         elif step['type'] == 'string':
-            for key in step['keys']:
-                keyboard.write(key)
+            for key_list in step['keys']:
+                for key in key_list:
+                    send_key(key)
+                    time.sleep(0.000000001)
+            send_key(NULL_CHAR)
         elif step['type'] == 'wait':
             seconds = 0
             if 'seconds' in step:
                 seconds = step['seconds']
             time.sleep(seconds)
-        elif step['type'] == 'mouse':
-            if 'absolute' in step:
-                absolute = True
-                x = step['absolute']['x']
-                y = step['absolute']['y']
-            else:
-                absolute = False
-                x = step['relative']['x']
-                y = step['relative']['y']
-            duration = 0
-            if 'duration' in step:
-                duration = step['duration']
-            keyboard.mouse.move( x, y, absolute, duration )
+        # elif step['type'] == 'mouse':
+        #     if 'absolute' in step:
+        #         absolute = True
+        #         x = step['absolute']['x']
+        #         y = step['absolute']['y']
+        #     else:
+        #         absolute = False
+        #         x = step['relative']['x']
+        #         y = step['relative']['y']
+        #     duration = 0
+        #     if 'duration' in step:
+        #         duration = step['duration']
+        #     keyboard.mouse.move( x, y, absolute, duration )
+
+def send_key( key ):
+    print(f'sending {key}')
+    if key in transform_key:
+        report = NULL_CHAR*2 + transform_key[key] + NULL_CHAR*5
+        with open('/dev/hidg0', 'rb+') as fd:
+            fd.write(report.encode())
 
 if __name__ == '__main__':
     # Parse the yaml and create a dictionary
@@ -40,17 +115,13 @@ if __name__ == '__main__':
 
     macros = yaml.safe_load(document)['macros']
 
-    # Verify the macros
+    # Setup multiprocessing
+    q = mp.Queue()
+    p1 = mp.Process(target=send_thread, args=(q, macros,))
+    p2 = mp.Process(target=receive_thread, args=(q,))
 
+    p1.start() 
+    p2.start() 
 
-    # Register the macros
-    print(macros)
-    for key, steps in macros.items():
-        keyboard.add_hotkey(key, handle_macro, args=[steps])
-
-    # TODO: Probably don't want this in the final port
-    # Exit
-    keyboard.wait('esc')
-    print("Exiting")
-    keyboard.unhook_all()
-    keyboard.unhook_all_hotkeys()
+    p1.join()
+    p2.join()
---
+++ read_keypad.py
@@ -0,0 +1,84 @@
+import RPi.GPIO as GPIO
+import time
+from datetime import datetime, timedelta
+
+L1 = 12
+L2 = 16
+L3 = 20
+L4 = 21
+
+C1 = 6
+C2 = 13
+C3 = 19
+C4 = 26
+
+LAST_KEY_TIMEOUT = timedelta(seconds = 1)
+
+def init_keypad():
+    # Initialize the GPIO pins
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(L1, GPIO.OUT)
+    GPIO.setup(L2, GPIO.OUT)
+    GPIO.setup(L3, GPIO.OUT)
+    GPIO.setup(L4, GPIO.OUT)
+
+    GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def readLine(line, characters):
+    GPIO.output(line, GPIO.HIGH)
+    ret_char = None
+    if(GPIO.input(C1) == 1):
+        ret_char = characters[0]
+    if(GPIO.input(C2) == 1):
+        ret_char = characters[1]
+    if(GPIO.input(C3) == 1):
+        ret_char = characters[2]
+    if(GPIO.input(C4) == 1):
+        ret_char = characters[3]
+    GPIO.output(line, GPIO.LOW)
+    return ret_char
+
+def read_lines():
+    # call the readLine function for each row of the keypad
+    # if valid return, save it
+    ret_char = None
+    temp_char = readLine(L1, ["1","2","3","A"])
+    if temp_char:
+        ret_char = temp_char
+    temp_char = readLine(L2, ["4","5","6","B"])
+    if temp_char:
+        ret_char = temp_char
+    temp_char = readLine(L3, ["7","8","9","C"])
+    if temp_char:
+        ret_char = temp_char
+    temp_char = readLine(L4, ["*","0","#","D"])
+    if temp_char:
+        ret_char = temp_char
+
+    return ret_char
+
+def receive_thread( queue ):
+    last_key = None
+    last_key_time = 0
+    init_keypad()
+    try:
+        while True:
+            time.sleep(0.1)
+            ret_char = read_lines()
+            if ret_char:
+
+                # Debounce the input more
+                if ret_char == last_key and ( datetime.now() - last_key_time ) < LAST_KEY_TIMEOUT:
+                    continue
+                last_key = ret_char
+                last_key_time = datetime.now()
+
+                print(f'Putting {ret_char}')
+                queue.put( ret_char )
+    except KeyboardInterrupt:
+        print("\nApplication stopped!")
